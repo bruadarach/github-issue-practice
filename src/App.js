1806c68702d0ef08421e1e172508737705eb9081
Sujeong Ji <suji.sujeongji@gmail.com>
2022-08-22 13:40:09 +0900

    feat: add image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
-import "./App.css";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  text-align: center;
+  display: flex;
+`;
 
 const App = () => {
-  return <div>App</div>;
+  const style = {
+    width: "250px",
+    height: "250px",
+    borderRadius: "50%",
+    margin: "1rem",
+  };
+
+  return (
+    <Wrapper>
+      <Title>App is running now!</Title>
+      <img
+        src={process.env.PUBLIC_URL + "/image/lucky.png"}
+        alt="angel"
+        style={style}
+      ></img>
+    </Wrapper>
+  );
 };
 
 export default App;
